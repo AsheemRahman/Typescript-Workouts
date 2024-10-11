4ca1ddbf9085db0057c52b6213276604e038a208
Asheem Rahman <mhdasheem@gmail.com>
2024-10-11 14:48:11 +0530

    add question

--- src/Question26.ts
+++ src/Question26.ts
@@ -1,7 +1,6 @@
 
 "26. Given a variable named my_string, try reversing the string using  my_string.split().reverse().join() and then print the reversed string to the console. If the try clause has an error, print the error message to the console. Finally, print the typeof of the my_string variable to the console."
 
-
 let my_string: string;
 
 my_string = 'hello'
